b05c64cea3ab6730d5fef86725ad245f30e70e8e
khinzen <krishan_hinzen@brown.edu>
2024-03-11 10:16:34 -0400

    Time Log 1
    Added Hour

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # eeg-project
 CLPS 0950 First Project
+
+Krishan Hour Log:
+
+1.5 Hr Sunday examining dataset and EEG Lab
--- sample.m
+++ sample.m
@@ -1,7 +1,3 @@
-grades = randn(5,9)*3+90
-grades(3)
-
-mean(grades,2) % computes across dimension
-mean(grades, 'all') % computes across all values in matrix
-
-% match name of file to function
+function [output] = sample(a,b)
+    output = (a + b + c)/2;
+end
